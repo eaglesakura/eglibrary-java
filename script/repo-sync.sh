4177335867a8a99d58263fd1c3d9f6cad6f6857a
eaglesakura <eagle.sakura@gmail.com>
2017-05-24 11:52:14 +0900

    同期対象のモジュール指定を追加

--- script/repo-sync.sh
+++ script/repo-sync.sh
@@ -23,21 +23,21 @@ repo_sync "android-camera"              "v1.3.x"        # updated version
 repo_sync "android-command-service"     "v1.5.x"        # updated version
 repo_sync "android-commons"             "v2.3.x"        # updated version
 repo_sync "android-devicetest-support"  "v2.3.x"        # updated version
-repo_sync "android-firebase"            "v1.10.2.x"
-repo_sync "android-gms"                 "v1.10.2.x"
+repo_sync "android-firebase"            "v1.10.3.x"     # updated version
+repo_sync "android-gms"                 "v1.10.3.x"     # updated version
 repo_sync "android-text-kvs"            "v2.4.x"        # updated version
 repo_sync "android-unittest-support"    "v1.6.x"        # updated version
-repo_sync "cerberus"                    "v2.4.x"
-repo_sync "garnet"                      "v1.1.x"
+repo_sync "cerberus"                    "v2.5.x"        # updated version
+repo_sync "garnet"                      "v1.2.x"        # updated version
 repo_sync "geo-utils"                   "v1.2.x"        # updated version
-repo_sync "greendao-wrapper"            "v2.3.x"
+repo_sync "greendao-wrapper"            "v2.4.x"        # updated version
 repo_sync "java-commons"                "v2.1.x"        # updated version
 repo_sync "json-wrapper"                "v1.2.x"        # updated version
 repo_sync "junit-support"               "v1.2.x"        # updated version
-repo_sync "light-saver"                 "v1.3.x"
-repo_sync "margarineknife"              "v1.4.x"
-repo_sync "onactivityresult-invoke"     "v1.2.x"
+repo_sync "light-saver"                 "v1.4.x"        # updated version
+repo_sync "margarineknife"              "v1.5.x"        # updated version
+repo_sync "onactivityresult-invoke"     "v1.3.x"        # updated version
 repo_sync "publd-serializer"            "v2.1.x"        # updated version
 repo_sync "simple-logger"               "v2.1.x"        # updated version
 repo_sync "sloth-framework"             "v3.25.2.x"     # updated version
-repo_sync "small-aquery"                "v1.2.x"
+repo_sync "small-aquery"                "v1.3.x"        # updated version
